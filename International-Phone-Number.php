43cb1ba8d85cb287cfac8cc14464eee0a2ab4b6a
Ian <mypetertw@gmail.com>
2019-10-18 17:38:59 +0800

    fix

--- International-Phone-Number.php
+++ International-Phone-Number.php
@@ -1,10 +1,10 @@
 <?php
 function STR_REPLACE_INTERNATIONAL($FROM, $TO, $NUMBER, $RANGE) {
-  $FROM = '/'.preg_quote($FROM, '/').'/';
+  $FROM = '/' . preg_quote($FROM, '/') . '/';
   return preg_replace($FROM, $TO, $NUMBER, $RANGE);
 }
 
 function STR_REPLACE_RESTORE($FROM, $TO, $NUMBER, $RANGE) {
-  $FROM = '/'.preg_quote($FROM, '/').'/';
+  $FROM = '/' . preg_quote($FROM, '/') . '/';
   return preg_replace($FROM, $TO, $NUMBER, $RANGE);
 }
